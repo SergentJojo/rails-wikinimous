39957a812b898f2de1622520389a6a8f38069cc1
Jonathan Dubois <dubois.bjonathan@gmail.com>
2021-11-09 17:01:12 +0100

    wikinimous on track

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+# require './app/models/article'
+
+p "start seeding"
+
+100.times do
+  Article.create(title: Faker::Sports::Football.player, content: Faker::Quote.famous_last_words)
+end
+
+
+p "end seeding"
